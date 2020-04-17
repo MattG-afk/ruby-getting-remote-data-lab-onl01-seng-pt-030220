91cf6a993b08ee15edb271c95c280af29c8498b8
Matt Gordon <bigmattsize21@gmail.com>
2020-04-17 20:59:07 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,6 +5,10 @@ require 'json'
 
 class GetRequester
   
+  def initialize(url)
+    @url = url
+  end
+  
   def get_response_body
     uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
